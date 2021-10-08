050221c2b3b25b69520363b8927df4ffdc328675
BondPetra <48263696+BondPetra@users.noreply.github.com>
2021-10-08 14:28:55 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+//unitのクリック計算
+var unit_check = document.getElementsByName('unit');
+
+for(i=0;i<unit_check.length;i++){
+    unit_check[i].addEventListener('click', function(e){
+        price_sum();
+    });
+}
+
+//合計計算
+document.getElementById('submit').onclick = 
+function price_sum(){
+    var sum = 0;
+    for(var i = 0 ; i < option_check.length ; i ++){
+      if(option_check[i].checked){
+        sum += parseInt(option_check[i].value);
+      }
+    }
+    for (var i = 0; i < unit_check.length; i++){
+        if(unit_check[i].checked){
+            sum += parseInt(unit_check[i].value);
+        }
+    }
+
+var goal_day = document.getElementById('goal').value;
+var gal = parseInt(goal_day);
+console.log(sum);
+// document.myform.total.value=sum/gal;
+total = document.getElementById("result");
+total.innerHTML = '一日　'+ sum/gal +'　個ずつ進めれば終わります！' + '<br>[切り上げ] ' + Math.ceil(sum/gal) + "  個進めよう！";
+// document.myform.safty.value=Math.ceil(sum/gal);
+}
